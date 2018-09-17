0772331a0a5128dda01ff0dc5879258bc6063acb
mholsey <mholsey@265bcc82-57df-11dd-8522-2d0c19f12d9e>
2018-09-17 12:09:54 +0000

    Updated FIO Script
    git-svn-id: https://quarchserver4.ds.quarch.com/svn/Software/Application%20Notes/AN-017%20-%20QPS%20performance%20test%20with%20FIO/Trunk@2569 265bcc82-57df-11dd-8522-2d0c19f12d9e

--- scriptFIO.py
+++ scriptFIO.py
@@ -1,19 +1,43 @@
-import time
+import time, os
 from quarchpy import ( requiredQuarchpyVersion,
-                     isQpsRunning, startLocalQps, quarchQPS, qpsInterface, 
+                     isQpsRunning, startLocalQps, quarchQPS, qpsInterface, GetQpsModuleSelection, 
                      quarchDevice,
-                     runFIO,)
+                     runFIO)
 
-if not requiredQuarchpyVersion ("1.3.4"):
-            raise ValueError ("quarchpy reported version is not new enough for this script!")
+#if not requiredQuarchpyVersion ("1.3.4"):
+#            raise ValueError ("quarchpy reported version is not new enough for this script!")
+
+def checkUserInput(userVar):
+    if userVar is None:
+        raise ValueError('\nPlease set up this variable at the start of the main function.\n')
 
 # Path where stream will be saved
-streamPath = "C:\Users\pleao\Desktop\quarchpy"
+streamPath = os.path.dirname(os.path.realpath(__file__))
+checkUserInput(streamPath)
 
-# Module to work with
-myDeviceID = "usb:QTL1999-02-001"
+# Location of .fio file
+fioFile = os.getcwd() + "\jobFileExample.fio"
+checkUserInput(fioFile)
+
+# Launch FIO in selected mode [arg|file]
+#    - arg : will use a dictionary (or list of dictionaries) to run one (or several) FIO jobs for each variable
+#    - file : will use a .fio file to load the arguments
+runMode = "arg"
+checkUserInput(fioFile)
+
+
 
 def main():
+
+    if 'filename' in open(fioFile).read():
+        print("This script will not work as intended with the argument \'filename\' in file: " + fioFile)
+        return
+
+    # Display title text
+    print ("\n################################################################################")
+    print ("\n                           QUARCH TECHNOLOGY                        \n\n  ")
+    print ("Automated power and performance data acquisition with Quarch Power Studio.   ")
+    print ("\n################################################################################\n")  
 
     # Checks is QPS is running on the localhost
     if not isQpsRunning():
@@ -21,7 +45,10 @@ def main():
         startLocalQps(keepQisRunning=True)
 
     # Open an interface to local QPS
-    myQps = qpsInterface()   
+    myQps = qpsInterface()
+    
+    # Module to work with
+    myDeviceID = GetQpsModuleSelection (myQps)
 
     # Create a Quarch device connected via QPS
     myQuarchDevice = quarchDevice (myDeviceID, ConType = "QPS")
@@ -42,7 +69,6 @@ def main():
     # Set the averaging rate to the module
     myQpsDevice.sendCommand ("record:averaging " + averaging)
 
-    #TODO: remove hardcoded path 
     # Start a stream, using the local folder of the script and a time-stamp file name in this example
     fileName = time.strftime("%Y-%m-%d-%H-%M-%S", time.gmtime())        
     myStream = myQpsDevice.startStream (streamPath + "\\" + fileName)
@@ -56,46 +82,56 @@ def main():
     fioCallbacks = {"TEST_START": notifyTestStart,
                     "TEST_END": notifyTestEnd,
                     "TEST_RESULT": notifyTestPoint}
-    #TODO: ju533a
-    #- create a way to add more than one job by creating more than one varible
-    #- create a "global" variable type  
-    arguments = {"directory":"D\:\TEST", 
-                "rw":"randread",           
-                "size":"128m",             
-                "runtime":"5",             
-                "output":"testoutput_file",
-                "status-interval":"1",     
-                "name":"job1"}
     
-    ##job2 = {"directory":"D\:\TEST", 
-    #            "rw":"randread",           
-    #            "size":"128m",             
-    #            "runtime":"5",             
-    #            "output":"testoutput_file",
-    #            "status-interval":"1",     
-    #            "name":"job2"}
+  
+    #Change / add arguments as required. JOB name should always be last added
+    arguments = {"directory":"G\:\Testing", 
+               "rw":"randread",           
+               "size":"128m",             
+               "runtime":"5",             
+               "output":"testFile",
+               "status-interval":"1",     
+               "name":"job1"}
 
-    #arguments = [job1, job2]
 
-    # TODO: implement a way to switch between modes - consider tuple of dictionaries and order of arguments in start_fio                                      
-    runFIO(myStream,                      
-           fioCallbacks,                  
+    #job2 = {"directory":"G\:\Testing", 
+    #           "rw":"randread",           
+    #           "size":"128m",             
+    #           "runtime":"5",             
+    #           "output":"mattsfile",
+    #           "status-interval":"1",     
+    #           "name":"job2"}
+
+
+    #arguments={job1,job2}
+                                 
+    runFIO(myStream,
+           runMode,
+           fioCallbacks,
+           user_data,
            arguments,                     
-           user_data)                     
+           fioFile)                     
 
     # End the stream after a few seconds of idle
     time.sleep(5)
+
     myStream.stopStream()
 
 def notifyTestStart (myStream, timeStamp, testDescription):
-    myStream.addAnnotation("<<text>TEST START</text><extraText>"+testDescription+"</extraText>>", timeStamp)
+    myStream.addAnnotation("<<text>TEST STARTED</text><extraText>" + testDescription + "</extraText>>", timeStamp)
+
 
 def notifyTestEnd (myStream, timeStamp, testName="END"):
+    #breaking data input to graph between tests
+    myStream.addDataPoint('read_iops', 'IOPS', "endSeq" , timeStamp )
+    myStream.addDataPoint('write_iops', 'IOPS', "endSeq" , timeStamp)
+
     myStream.addAnnotation(testName, timeStamp)
 
 def notifyTestPoint (myStream, timeStamp, dataValues):
-        myStream.addDataPoint('read_iops', 'IOPS', dataValues['read_iops'], timeStamp)
-        myStream.addDataPoint('write_iops', 'IOPS', dataValues['write_iops'], timeStamp)
+    myStream.addDataPoint('read_iops', 'IOPS', dataValues['read_iops'], timeStamp)
+    myStream.addDataPoint('write_iops', 'IOPS', dataValues['write_iops'], timeStamp)
+
 
 def setupPowerOutput (myModule):
     # Output mode is set automatically on HD modules using an HD fixture, otherwise we will chose 5V mode for this example
