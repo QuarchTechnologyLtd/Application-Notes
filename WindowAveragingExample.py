f00f1c0e6648436050674301d3b7d85d1d9b3fd0
anorrie <anorrie@265bcc82-57df-11dd-8522-2d0c19f12d9e>
2021-10-20 07:47:16 +0000

    Speed increased by using a rolling sum rather than summing the deque on each cycle
    git-svn-id: https://quarchserver4.ds.quarch.com/svn/Software/Application%20Notes/AN-025%20-%20QPS%20Window%20Averaging%20Example@5211 265bcc82-57df-11dd-8522-2d0c19f12d9e

--- WindowAveragingExample.py
+++ WindowAveragingExample.py
@@ -65,7 +65,8 @@ Assumes the first column is the time data
 '''
 def active_power_calc (data_path, col_name="5V power uW", window=1000000000, csv_delimiter=","):
     
-    worst_case = 0
+    worst_case = 0    
+    sum_value = 0
     
     # Quote out the column name, as in the csv
     col_name = "\"" + col_name + "\""
@@ -96,7 +97,7 @@ def active_power_calc (data_path, col_name="5V power uW", window=1000000000, csv
     window_samples = int(window / time_step)     
     if (window_samples == 0):
         raise ValueError ("Window size of 0 stripes calculated, check your window parameter")
-    window_sample_data = deque()
+    window_sample_data = deque(maxlen = window_samples)
     
     # Deal with unusual cases that window is 2 samples or less
     value1 = (int(data_line.split(csv_delimiter)[header_pos]))
@@ -107,31 +108,34 @@ def active_power_calc (data_path, col_name="5V power uW", window=1000000000, csv
         worst_case = Value1        
         if (value2 > worst_case):
             worst_case = value2
-    # Otherwise push the samples onto the window queue
+    # Otherwise push the samples onto the window queue and track the total
     else:
         window_sample_data.appendleft (value1)
-        window_sample_data.appendleft (value2)        
+        window_sample_data.appendleft (value2)  
+        sum_value = value1 + value2
     
     # Loop until the file is complete   
     data_line = data_line = file.readline ()
     while (data_line is not None):        
 
         # If the sample window is full, we have to pop the oldest value now
-        if (len(window_sample_data) == window_samples):
-            window_sample_data.pop()
+        # We also subtract this from the sum of all points (this avoids summing the whole window every cycle)
+        if (len(window_sample_data) == window_samples):            
+            sum_value = sum_value - window_sample_data.pop()
     
         # Read the next data element
         value1 = int(data_line.split (csv_delimiter)[header_pos])
         # Add it to the window data
         window_sample_data.appendleft (value1)
-        
-        # Sum the values in the queue and track the worst case average
-        window_average = sum(window_sample_data) / len(window_sample_data)
+        sum_value = sum_value + value1
+                
+        # Sum the values in the queue and track the worst case average       
+        window_average = sum_value / len(window_sample_data)
         if (window_average > worst_case):
-            worst_case = window_average
+            worst_case = window_average                   
 
         # Read the next line in       
-        data_line = data_line = file.readline ()
+        data_line = file.readline ()
         if (data_line == ''):
             break                
             
