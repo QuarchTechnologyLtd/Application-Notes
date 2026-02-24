500f45873ba034ad5ef1cf34ba834bd98e5388bc
AndrewS-Quarch <andrew.steedman@quarch.com>
2026-02-24 12:52:25 +0000

    Committing as is. Making changes to be able to time the difference between stream starts

--- Application_Notes/AN-034_Multi_AC_PAM_Streaming/MultiPAM_Stream_Simple.py
+++ Application_Notes/AN-034_Multi_AC_PAM_Streaming/MultiPAM_Stream_Simple.py
@@ -1,15 +1,50 @@
+"""
+This script is a simple way of streaming from 2 PAMs together. These can either be standard DC PAMs, or can be AC PAMs
+This streams over QIS, then has the option to combine both datasets together, and view in QPS.
+
+This is a simpler, more user-friendly way of operating multiple PAMs. Through testing, the hardware trigger has a delay of
+, the sequential start has a delay of , and the simpler multiprocessing has a delay of .
+If shorter delays are required, see MultiPAM_Stream_Complex. Reducing the delay adds complexity.
+
+The PAM streams can either be started with the PAMs connected by a triggering cable,or a software trigger.
+If using DC PAMs (QTL2312) suggested to use a triggering cable
+For PAMs without a triggering cable (e.g. AC PAMs), you will need to use a software trigger.
+
+########### VERSION HISTORY ###########
+24/02/2026 - Andrew Steedman - Separated from MultiPAM_Stream_Complex.py
+
+########### REQUIREMENTS ##############
+
+1 - Python (3.x recommended)
+    https://www.python.org/downloads/
+2 - Quarchpy python package
+    https://quarch.com/products/quarchpy-python-package/
+3- A multicore processor (2 minimum)
+
+########### INSTRUCTIONS ##############
+
+1 - Setup the PAM Fixture
+2 - Connect the PAMs to the PC
+3 - Run the script
+"""
+
 import multiprocessing
 import os
 import time
 from datetime import datetime
 
 import quarchpy
-from quarchpy.device import quarchPPM
+from quarchpy.device import quarchPPM, get_quarch_device
 from quarchpy.qis import *
 from quarchpy.qps import startLocalQps
 from quarchpy.user_interface import showYesNoDialog, showDialog, visual_sleep
 import SyncUtils
 
+#Length of stream in seconds
+STREAM_LENGTH = 10
+
+#Resample rate
+RESAMPLE_RATE = "1ms"
 
 def main():
     # # If you require logging, quarchpy logs everything level debug and above to file. It is also set to log to console
@@ -22,21 +57,23 @@ def main():
     print("*****************************************")
     print("*****************************************\n")
     print("AN-034 - Multiple PAM Synchronous Stream")
-    print("Connect the devices over IP on the same network")
+    print("Connect the devices to the same host PC")
     print("\n*****************************************")
     print("*****************************************")
 
     if not isQisRunning():
-        print("Starting QIS")
+        print("Starting QIS...")
         myQis = startLocalQis()
 
     # The return from the module selection is a device ID string that we can use to connect to.
     # If you know the name of the module you would like to talk to, then you can skip module selection and
     # hardcode the string using the serial number or IP address
     my_pam_1 =  myQis.GetQisModuleSelection(additionalOptions=['Rescan', 'All Con Types', 'Ip Scan','Quit'])
+
     print("PAM 1 is: " + my_pam_1 + "\n")
 
     my_pam_2 =  myQis.GetQisModuleSelection(additionalOptions=['Rescan', 'All Con Types', 'Ip Scan','Quit'])
+
     print("PAM 2 is: " + my_pam_1 + "\n")
 
     # The return from the module selection is a device ID string that we can use to connect to.
@@ -46,11 +83,19 @@ def main():
     # my_pam_2 = "TCP:192.168.1.25"
 
     # Upgrades the PAMs to PPM devices - more features
-    pam_1_power_device = quarchPPM(my_pam_1)
-    pam_2_power_device = quarchPPM(my_pam_2)
+    my_pam_1_device = get_quarch_device(my_pam_1, ConType="QIS")
+    my_pam_2_device = get_quarch_device(my_pam_2, ConType="QIS")
+
+    #Upgrades quarch device to quarchPPM - Power product class with more features
+    pam_1_power_device = quarchPPM(my_pam_1_device)
+    pam_2_power_device = quarchPPM(my_pam_2_device)
+
+    #Sets the stream resample rate to RESAMPLE_RATE (Default 1ms) across both devices
+    pam_1_power_device.send_command(f"stream mode resample {RESAMPLE_RATE}")
+    pam_2_power_device.send_command(f"stream mode resample {RESAMPLE_RATE}")
 
     #Asks the user if they are using a hardware trigger
-    hardware_trigger = showYesNoDialog(title="Are you using a triggering cable between the PAMs?", message="Yes if you are using a triggering cable between the PAMs")
+    hardware_trigger = showYesNoDialog(title="Are you using a triggering cable between the PAMs?", message="Are using a triggering cable between the PAMs?")
     if hardware_trigger == "Yes":
         #Provides instructions on how to connect the units
         print(f"Connect PAM 1 ({my_pam_1}) trigger out, to PAM 2 ({my_pam_2}) trigger in")
@@ -64,18 +109,22 @@ def main():
         #Configures PAM1 to output a trigger record start
         pam_1_power_device.send_command("TRIGger:OUT:MODE RECORD")
 
+        time.sleep(1)
+
         print("Trigger set-up")
 
         #Setups stream on PAM2, with the target CSV - waits on trigger
         pam_2_power_device.start_stream("RawDataPam2.csv")
 
+        time.sleep(1)
+
         #Starts stream on PAM 1, which will fire the trigger on PAM2, with a length of 60 seconds
-        pam_1_power_device.start_stream("RawDataPam1.csv", stream_duration=60)
+        pam_1_power_device.start_stream("RawDataPam1.csv", stream_duration=STREAM_LENGTH)
 
         print("Stream started")
 
         #Waits stream_length
-        visual_sleep(60)
+        visual_sleep(STREAM_LENGTH)
 
         #Stop PAM2 after Stream Length - trigger only syncs the start
         pam_2_power_device.send_command("RECord:STOP")
@@ -83,15 +132,18 @@ def main():
         print("Stream completed")
 
     else: #Use a software trigger
-        #Simplest option - start them sequentially - Highest delay
-        my_pam_1.start_stream("RawDataPam1.csv", stream_duration=60)
-        my_pam_2.start_stream("RawDataPam2.csv", stream_duration=60)
+        #Simplest option - start them sequentially
+        pam_1_power_device.start_stream("RawDataPam1.csv", stream_duration=STREAM_LENGTH)
+        pam_2_power_device.start_stream("RawDataPam2.csv", stream_duration=STREAM_LENGTH)
+        visual_sleep(STREAM_LENGTH)
+        print("Streaming...")
+
 
         #This is a quicker way of starting the stream - Uses 2 CPU cores, that are kept busy, and the
-        #pam command to start streaming is waiting to jump in - significantly quicker
+        #pam command to start streaming is waiting to jump in - quicker but more complex
         #Uncomment this, and comment in the 2 lines above if wanting to use
 
-        #spin_cpu_and_start_stream(pam_1_power_device, pam_2_power_device)
+        #SyncUtils.spin_cpu_and_start_stream(my_pam_1, my_pam_2, RESAMPLE_RATE, STREAM_LENGTH)
 
     #Stream is complete
     #Asks user if they want to combine data and display it in QPS
@@ -102,57 +154,10 @@ def main():
     if post_process == "Yes":
         #If yes, combine the 2 csvs into 1
         #Combines the CSVs with a shared time column
-        combined_csv = SyncUtils.csv_combiner("RawDataPam1.csv", "RawDataPam2.csv")
+        combined_csv_path = SyncUtils.csv_combiner("RawDataPam1.csv", "RawDataPam2.csv")
 
-        #And then open in QPS
-        SyncUtils.view_csv_in_qps(combined_csv)
-
-
-def spin_cpu_and_start_stream(pam_1_power_device: quarchPPM, pam_2_power_device: quarchPPM):
-    """
-    Uses multiple CPU cores to keep busy, and then release at a set point, in effort to reduce the time needed to start stream
-
-    :param pam_1_power_device: PAM1
-    :param pam_2_power_device: PAM2
-
-    Returns: None
-    """
-    # More complex but much quicker
-    # Changes what clock is used depending on windows or posix
-    clock_id = time.CLOCK_MONOTONIC if os.name == "POSIX" else None
-
-    # Gets the current time in nanoseconds
-    now = time.clock_gettime_ns(clock_id) if clock_id else time.time_ns()
-
-    # 3 seconds in the future (in nanoseconds)
-    target_ns = now + int(3 * 1e9)
-
-    # Uses 1 core per PAM, keeps CPU busy until time starts
-    #Passes in the PAM, file to save data to, and the time to spin until
-    process1 = multiprocessing.Process(target=SyncUtils.spin_cpu_simple,
-                                       args=(pam_1_power_device, "RawDataPam1.csv", target_ns))
-    process2 = multiprocessing.Process(target=SyncUtils.spin_cpu_simple,
-                                       args=(pam_2_power_device, "RawDataPam2.csv", target_ns))
-    try:
-        #Starts the process's activity
-        process1.start()
-        process2.start()
-
-        #Blocks the main script until both processes are done
-        process1.join()
-        process2.join()
-
-    #If user exits (e.g. Ctrl+C) safely close the processes)
-    except KeyboardInterrupt:
-        #Checks if alive
-        if process1.is_alive():
-            #Close the process
-            process1.terminate()
-        if process2.is_alive():
-            process2.terminate()
-
-        #Exit the script
-        exit(0)
+        #And then convert and open in QPS
+        SyncUtils.view_csv_in_qps(combined_csv_path, my_pam_1)
 
 
 if __name__ == "__main__":
--- Application_Notes/AN-034_Multi_AC_PAM_Streaming/SyncUtils.py
+++ Application_Notes/AN-034_Multi_AC_PAM_Streaming/SyncUtils.py
@@ -1,3 +1,4 @@
+import multiprocessing
 from datetime import datetime
 import subprocess
 import time
@@ -7,9 +8,7 @@ import os
 
 from quarchpy.connection_specific.connection_QPS import QpsInterface
 from quarchpy.qps import startLocalQps
-from quarchpy.device import quarchPPM
-
-
+from quarchpy.device import quarchPPM, get_quarch_device
 
 
 def csv_combiner(csv_file_1:str, csv_file_2:str):
@@ -37,33 +36,14 @@ def csv_combiner(csv_file_1:str, csv_file_2:str):
     #time, 1_B,1_C,..., 1_XXX, 2_B,2_C,...,2_XXX
     merged_data = pd.merge(csv1_prefix, csv2_prefix, on=shared_time_column, how="outer")
 
+    combined_csv = merged_data.to_csv("CombinedData.csv", index=False)
+
+    path = os.path.abspath(combined_csv)
     #Changes the dataframe to CSV
-    return merged_data.to_csv("CombinedData.csv", index=False)
+    print(f"CSV can be found :{path}")
+    return path
 
-def spin_cpu_simple(pam: quarchPPM, filename: str, target_ns: int):
-    """
-    Used in the simple version to spin up the CPU core, using python only, and then start stream when a time has passed
-    :param pam: PAM - The PAM to stream
-    :param filename: The filename to save data to
-    :param target_ns: Target time to spin up until
-    Returns: None
-    """
-    # Change the clock to account for epoch differences on OSs
-    clock_id = time.CLOCK_MONOTONIC if os.name == "POSIX" else None
-
-
-    while True:  # Keep the CPU busy
-        # Get current time in nanoseconds
-        now = time.clock_gettime_ns(clock_id) if clock_id else time.time_ns()
-
-        #If we have reached the time we are waiting for
-        if now >= target_ns:
-            #Start stream
-            pam.start_stream(filename, stream_duration=60)
-            #Exit loop
-            break
-
-def view_csv_in_qps(csv_file:str, qps_instance: QpsInterface = None):
+def view_csv_in_qps(csv_path:str, pam_address:str, qps_instance: QpsInterface = None):
     """
     Opens QPS and reconnects to PAM 1
     Used for user to view the CSVs as QPS Traces
@@ -75,16 +55,17 @@ def view_csv_in_qps(csv_file:str, qps_instance: QpsInterface = None):
     """
 
     if qps_instance is None:
+        print("Opening QPS...")
         qps_instance = startLocalQps()
 
-    current_time = datetime.now()
-    formatted_time = current_time.strftime("_%H_%M_%S")
+    #Connects to the pam specified
+    qps_instance.connect(pam_address)
 
     file_path = os.getcwd() + rf"\sync_stream\sync_stream.qps"
 
     print("Converting CSV file to QPS")
 
-    command = f'$convert csv from="{csv_file}" to="{file_path}"'
+    command = f'$convert csv from="{csv_path}" to="{file_path}"'
     response = qps_instance.sendCommand(command)
     print(response)
 
@@ -93,6 +74,91 @@ def view_csv_in_qps(csv_file:str, qps_instance: QpsInterface = None):
     command = f'$open recording qpsFile="{file_path}"'
     response = qps_instance.sendCommand(command)
     print(response)
+
+def spin_cpu_simple(pam_address: str, filename: str, target_ns: int, resample_rate:str, stream_length: int):
+    """
+    Used in the simple version to spin up the CPU core, using python only, and then start stream when a time has passed
+    :param pam_address: PAM - The IP address of the PAM to stream
+    :param filename: The filename to save data to
+    :param target_ns: Target time to spin up until
+    :param resample_rate: Resampling rate to use
+    :param stream_length: Stream length to use
+
+    Returns: None
+    """
+    # Change the clock to account for epoch differences on OSs
+    clock_id = time.CLOCK_MONOTONIC if os.name == "POSIX" else None
+
+    #Connects to the PAM - Pickling error otherwise
+    pam = get_quarch_device(pam_address, ConType="QIS")
+    #Upgrades to PPM class
+    pam = quarchPPM(pam)
+
+    pam.send_command(f"stream mode resample {resample_rate}")
+
+    while True:  # Keep the CPU busy
+        # Get current time in nanoseconds
+        now = time.clock_gettime_ns(clock_id) if clock_id else time.time_ns()
+
+        #If we have reached the time we are waiting for
+        if now >= target_ns:
+            #Start stream
+            pam.start_stream(filename, stream_duration=stream_length)
+            #Exit loop
+            break
+
+def spin_cpu_and_start_stream(pam_1_address: str, pam_2_address: str, resample_rate: str, stream_length: int):
+    """
+    Uses multiple CPU cores to keep busy, and then release at a set point, in effort to reduce the time needed to start stream
+
+    :param pam_1_address: PAM1
+    :param pam_2_address: PAM2
+    :param resample_rate: Resampling rate to use
+    :param stream_length: Stream length to use
+
+    Returns: None
+    """
+    # More complex but much quicker
+    # Changes what clock is used depending on windows or posix
+    clock_id = time.CLOCK_MONOTONIC if os.name == "POSIX" else None
+
+    # Gets the current time in nanoseconds
+    now = time.clock_gettime_ns(clock_id) if clock_id else time.time_ns()
+
+    # 3 seconds in the future (in nanoseconds)
+    target_ns = now + int(3 * 1e9)
+
+    # Uses 1 core per PAM, keeps CPU busy until time starts
+    #Passes in the PAM, file to save data to, and the time to spin until
+    process1 = multiprocessing.Process(target=spin_cpu_simple,
+                                       args=(pam_1_address, "RawDataPam1.csv", target_ns, resample_rate, stream_length))
+    process2 = multiprocessing.Process(target=spin_cpu_simple,
+                                       args=(pam_2_address, "RawDataPam2.csv", target_ns, resample_rate, stream_length))
+
+    time.sleep(2)
+
+    try:
+        #Starts the process's activity
+        process1.start()
+        process2.start()
+
+        #Blocks the main script until both processes are done
+        process1.join()
+        process2.join()
+
+    #If user exits (e.g. Ctrl+C) safely close the processes
+    except KeyboardInterrupt:
+        #Checks if alive
+        if process1.is_alive():
+            #Close the process
+            process1.terminate()
+        if process2.is_alive():
+            process2.terminate()
+
+        #Exit the script
+        exit(0)
+
+
 
 def ping_device(ip_address:str):
     """
